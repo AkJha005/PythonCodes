07bd52fe5a784e930631cdbb8181fac74816d49e
AkJha005 <24bpe063@spt.pdpu.ac.in>
2025-02-10 15:38:23 +0530

    Add files via upload

---
+++ 24BPE063.py
@@ -0,0 +1,25 @@
+#Prac-3 Q1
+a=int(input('Enter 1st No. :-'))
+b=int(input('Enter 2nd No. :-'))
+if a>b:
+  print(a,'is Bigger')
+else:
+  print(b,'is Bigger')
+
+#Prac-3 Q2
+a=int(input('Enter 1st No. :-'))
+b=int(input('Enter 2nd No. :-'))
+c=int(input('Enter 3rd No. :-'))
+if a>b>c:
+  print(a,'is Bigger')
+elif b>c>a:
+  print(b,'is Bigger')
+else:
+  print(c,'is Bigger')
+
+#Prac-3 Q3
+a=int(input('Enter the No.'))
+if (a%2==0):
+  print('The No. is Even')
+else:
+  print('The No. is Odd')
